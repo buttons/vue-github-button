141c381fe4d725b0451bb335b50e8cc38c8e0f80
なつき <i@ntk.me>
2019-02-17 23:22:58 -0800

    github-buttons 2.0.0

--- index.js
+++ index.js
@@ -2,25 +2,40 @@ import { render } from 'github-buttons'
 
 export default {
   name: 'github-button',
-  props: ['href', 'title', 'ariaLabel', 'dataIcon', 'dataSize', 'dataText', 'dataShowCount'],
+  props: ['href', 'ariaLabel', 'title', 'dataIcon', 'dataSize', 'dataShowCount', 'dataText'],
   render: function (h) {
     return h('a', { attrs: {
-      href: this.href,
-      title: this.title,
+      'href': this.href,
       'aria-label': this.ariaLabel,
+      'title': this.title,
       'data-icon': this.dataIcon,
       'data-size': this.dataSize,
-      'data-text': this.dataText,
-      'data-show-count': this.dataShowCount
+      'data-show-count': this.dataShowCount,
+      'data-text': this.dataText
     } }, this.$slots.default)
   },
   mounted: function () {
-    render(this.$el.parentNode.insertBefore(this._ = document.createElement('span'), this.$el).appendChild(this.$el))
+    this.render()
   },
   beforeUpdate: function () {
-    this._.parentNode.replaceChild(this.$el, this._)
+    this.reset()
   },
   updated: function () {
-    render(this.$el.parentNode.insertBefore(this._ = document.createElement('span'), this.$el).appendChild(this.$el))
+    this.render()
+  },
+  beforeDestroy: function () {
+    this.reset()
+  },
+  methods: {
+    render: function () {
+      render(this.$el.parentNode.insertBefore(this._ = document.createElement('span'), this.$el).appendChild(this.$el), (function(_this) {
+        return function (el) {
+          _this.$el.parentNode.replaceChild(el, _this.$el)
+        }
+      })(this))
+    },
+    reset: function () {
+      this._.parentNode.replaceChild(this.$el, this._)
+    }
   }
 }
